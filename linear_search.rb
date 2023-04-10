9a622381558a5d13ca1fb0b71a49dc5dd4c0eed6
jun-kondo <ranmaru.oo.q@gmail.com>
2023-04-10 19:52:44 +0900

    線形探索

---
+++ linear_search.rb
@@ -0,0 +1,16 @@
+def linear_search(array)
+  search_number = 2
+  find_id = -1
+
+  array_number = array.length
+
+  array_number.times do |num|
+    if array[num] == search_number
+      find_id = num
+    end
+  end
+  puts "見つかった番号= #{find_id}"
+end
+
+arr = [4, 5, 2, 1, 3]
+linear_search(arr)
