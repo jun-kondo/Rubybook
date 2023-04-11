28f365176c9d423bfb43aff8b2ba3e9e3f92829a
jun-kondo <ranmaru.oo.q@gmail.com>
2023-04-11 20:25:48 +0900

    [fix]bingo問題

--- bingo.rb
+++ bingo.rb
@@ -1,9 +1,11 @@
-cols = (1..75).each_slice(15).map { |sequence| sequence.sample(5)}
-cols[2][2] = ""
-#print cols
+def card
+  bingo = ["BINGO".chars]
+  numbers = (1..75).each_slice(15).map { |number| number.sample(5) }.transpose
+  numbers[2][2] = "  "
+  rows = bingo + numbers
+  rows.map  { |row|
+    row.map { |r| r.to_s.rjust(2) }.join(" | ")
+  }.join("\n")
+end
 
-card = ["BINGO".chars] + cols.transpose
-
-card.map {|row|
-  print row.map { |v| v.to_s.rjust(2) }.join(' | ')
-}.join("\n")
+puts card
--- bingo_card.rb
+++ bingo_card.rb
@@ -1,9 +1,18 @@
-class Bingo
-  def self.generate_card
-    # 以下の行は削除して、自分でロジックを実装してください。
-    cols = (1..75).each_slice(15).map { |sequence| sequence.sample(5)}
-    cols[2][2] = ""
-    cols.transpose
-    print cols
-  end
+def card
+	#　一文字ずつ配列にする。
+	bingo = ["BINGO".chars]
+	numbers = (1..75).each_slice(15).map { |number| number.sample(5) }.transpose
+	#　真ん中を空白にする[3番目の配列][3番目の要素]
+	numbers[2][2] = "  "
+	#　配列を合体
+	rows = bingo + numbers
+	#　配列を並べ替えてjoinメソッド改行を区切り文字にして連結する
+	rows.map  { |row|
+	#　配列の要素を文字列にして2文字で右寄せ(rjust(2))にしたあと
+	#　"|"を区切り文字にしてjoinで連結　>　rowは配列から1つの文字列になる
+		row.map { |r| r.to_s.rjust(2) }.join(" | ")
+	}.join("\n")
 end
+
+#　最終的に1つの文字列を取得している。
+puts card
